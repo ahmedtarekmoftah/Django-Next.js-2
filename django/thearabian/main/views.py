c7d4050580a8da3a1bdc4bf9050ae9068248be05
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-17 18:44:43 +0200

    Update views.py

--- django/thearabian/main/views.py
+++ django/thearabian/main/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from main.models import country
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import permissions
 from .serializers import UserSerializer, GroupSerializer, CountrySerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = country.objects.all()
     serializer_class = CountrySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
