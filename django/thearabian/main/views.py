ef6eb23980f65b2d8953d59cdb2058dbe6a6b486
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-17 18:43:32 +0200

    Update views.py

--- django/thearabian/main/views.py
+++ django/thearabian/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from main.models import country
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import UserSerializer, GroupSerializer, CountrySerializer
 
 
