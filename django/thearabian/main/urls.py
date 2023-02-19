6610557ffb51771b5c02070eadb1ec2c74ad464d
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-19 20:27:37 +0200

    s

--- django/thearabian/main/urls.py
+++ django/thearabian/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 router = routers.DefaultRouter()
@@ -8,6 +9,7 @@ router = routers.DefaultRouter()
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('<int:pk>', views.country_detail_view),
--- django/thearabian/main/views.py
+++ django/thearabian/main/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from main.models import country
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, authentication
 from .serializers import UserSerializer, GroupSerializer, CountrySerializer
 
 
 class CountryDetailAPIView(generics.RetrieveAPIView):
     queryset = country.objects.all()
-    serializer_class = CountrySerializer
+    serializer_classes = CountrySerializer
+    permissions_classes = [permissions.IsAdminUser]
+    authentication_classes = [authentication.SessionAuthentication,
+                              authentication.TokenAuthentication]
 
 
 country_detail_view = CountryDetailAPIView.as_view()
