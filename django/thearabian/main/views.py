3af2a759636b38bafc94b56584c02b33d438f322
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-19 20:10:10 +0200

    dg

--- django/thearabian/main/views.py
+++ django/thearabian/main/views.py
@@ -1,44 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from main.models import country
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from .serializers import UserSerializer, GroupSerializer, CountrySerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class CountryViewSet(viewsets.ModelViewSet):
-    queryset = country.objects.all()
+class CountryDetailAPIView(generics.RetrieveAPIView):
+    queryset = Country.objects.all()
     serializer_class = CountrySerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
-def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(200)
-        else:
-            return render(request, "main/login.html", {
-                "message": "Invalid email and/or password."
-            })
-    else:
-        return render(request, 'main/login.html')
+country_detail_view = CountryDetailAPIView.as_view()
