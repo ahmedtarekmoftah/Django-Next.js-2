9954bbc1ae5e15df63e1be53eee2f3780be43ec7
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-20 18:59:23 +0200

    Update settings.py

--- django/thearabian/thearabian/settings.py
+++ django/thearabian/thearabian/settings.py
@@ -107,8 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 11,
+
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
         # 'rest_framework.permissions.IsAuthenticated',
