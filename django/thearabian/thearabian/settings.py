c9de32a3824255a7842e87ab9174d52d79f90edd
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-21 18:55:56 +0200

    s

--- django/thearabian/thearabian/settings.py
+++ django/thearabian/thearabian/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': '/thearabian/auth/mysql.cnf',
+            'charset': 'utf8mb4'
         },
     }
 }
